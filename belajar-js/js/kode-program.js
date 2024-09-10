4b21ecd9437e86659b7db33d0a7a37f393f80163
safrizalrahman45 <safrizalrahman46@gmail.com>
2024-09-10 08:49:45 +0700

    first commit

---
+++ belajar-js/js/kode-program.js
@@ -0,0 +1,6 @@
+alert("Hello, ini adalah program JS eksternal!");
+
+
+// <!-- Menyisipkan kode js eksternal -- >
+{/* <script src="kode-program.js"></script> */}
+<script src="https://www.petanikode.com/js/kode.js"></script>
