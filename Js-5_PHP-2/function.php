557bf3cd3c2eca06e67a316c6f9d0026d5a5a55d
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-09-24 08:38:48 +0700

    question 6

--- Js-5_PHP-2/function.php
+++ Js-5_PHP-2/function.php
@@ -12,22 +12,42 @@
 
 //Add Parameters
 // Membuat fungsi dengan parameter
-function perkenalan($nama, $salam) {
-    echo $salam . ", ";
-    echo "Perkenalkan, nama saya " . $nama . "<br/>";
+// function perkenalan($nama, $salam) {
+//     echo $salam . ", ";
+//     echo "Perkenalkan, nama saya " . $nama . "<br/>";
+//     echo "Senang berkenalan dengan Anda<br/>";
+// }
+
+// // Memanggil fungsi yang sudah dibuat
+// perkenalan("Hamdana", "Hallo");
+
+// echo "<hr>";
+
+// $saya = "Elok";
+// $ucapanSalam = "Selamat pagi";
+
+// // Memanggil lagi
+// perkenalan($saya, $ucapanSalam);
+
+// Add parameter dan using default value
+
+// Membuat fungsi dengan parameter dan nilai default
+function perkenalan ($nama, $salam="Assalamualaikum") {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama. "<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
-}
-
-// Memanggil fungsi yang sudah dibuat
-perkenalan("Hamdana", "Hallo");
-
-echo "<hr>";
-
-$saya = "Elok";
-$ucapanSalam = "Selamat pagi";
-
-// Memanggil lagi
-perkenalan($saya, $ucapanSalam);
-
+    
+    }
+    
+    //memanggil fungsi yang sudah dibuat
+    perkenalan ("Hamdana","Hallo");
+    
+    echo "<hr>";
+    
+    $saya = "Elok";
+    $ucapanSalam = "Selamat pagi";
+    
+    //memanggil lagi tanpa mengisi parameter salam
+    perkenalan ($saya);
 
 ?>
