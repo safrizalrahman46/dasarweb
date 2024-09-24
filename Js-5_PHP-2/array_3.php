f9cfdf7d443ee97986408d0602c9c51956f3b53f
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-09-24 09:59:52 +0700

    update question no 3

--- Js-5_PHP-2/array_3.php
+++ Js-5_PHP-2/array_3.php
@@ -22,13 +22,41 @@ $movie = array (
     array ("Iron Man", 2008, 7.9)
 );
 
-foreach ($movie as $m) {
-    echo "<tr>";
-    echo "<td>" . $m[0] . "</td>";
-    echo "<td>" . $m[1] . "</td>";
-    echo "<td>" . $m[2] . "</td>";
-    echo "</tr>";
-}
+echo "<tr>";
+echo "<td>". $movie [0] [0] . "</td>";
+echo "<td>". $movie [0] [1] . "</td>";
+echo "<td>". $movie [0] [2] . "</td>";
+echo "</tr>";
+
+
+echo "<tr>";
+echo "<td>". $movie [1] [0] ."</td>";
+echo "<td>". $movie [1] [1] . "</td>";
+echo "<td>". $movie [1] [2] . "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>". $movie [2] [0] . "</td>";
+echo "<td>". $movie [2] [1] . "</td>";
+echo "<td>". $movie [2] [2] . "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>". $movie [3] [0] . "</td>";
+echo "<td>". $movie [3] [1] . "</td>";
+echo "<td>". $movie [3] [2] . "</td>";
+echo "</tr>";
+
+// memakai foreach
+// foreach ($movie as $m) {
+//     echo "<tr>";
+//     echo "<td>" . $m[0] . "</td>";
+//     echo "<td>" . $m[1] . "</td>";
+//     echo "<td>" . $m[2] . "</td>";
+//     echo "</tr>";
+
+
+// }
 ?>
 
 </table>
