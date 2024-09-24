05bf89fa85174b3d2dd9b62b0a60f55cc73648a2
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-09-24 08:59:34 +0700

    question 12

--- Js-5_PHP-2/multi_menu.php
+++ Js-5_PHP-2/multi_menu.php
@@ -1,4 +1,55 @@
 <?php
+// $menu = [
+//     [
+//         "nama" => "Beranda"
+//     ],
+//     [
+//         "nama" => "Berita",
+//         "subMenu" => [
+//             [
+//                 "nama" => "Wisata",
+//                 "subMenu" => [
+//                     [
+//                         "nama" => "Pantai"
+//                     ],
+//                     [
+//                         "nama" => "Gunung"
+//                     ]
+//                 ]
+//             ]
+//         ]
+//     ],
+//     [
+//         "nama" => "Kuliner"
+//     ],
+//     [
+//         "nama" => "Hiburan"
+//     ],
+//     [
+//         "nama" => "Tentang"
+//     ],
+//     [
+//         "nama" => "Kontak"
+//     ]
+// ];
+
+// // Function to display the multi-level menu
+// function tampilkanMenuBertingkat(array $menu) {
+//     echo "<ul>";
+//     foreach ($menu as $item) {
+//         echo "<li>{$item['nama']}</li>";
+//         // Check if the current item has a subMenu
+//         if (isset($item['subMenu'])) {
+//             tampilkanMenuBertingkat($item['subMenu']); // Recursive call
+//         }
+//     }
+//     echo "</ul>";
+// }
+
+// // Call the function to display the menu
+// tampilkanMenuBertingkat($menu);
+
+//question 12
 $menu = [
     [
         "nama" => "Beranda"
@@ -14,17 +65,60 @@ $menu = [
                     ],
                     [
                         "nama" => "Gunung"
+                    ],
+                    
+                ]
+                ],
+                [
+                    "nama" => "Kuliner",
+                    "subMenu" => [
+                        [
+                            "nama" => "Pantai"
+                        ],
+                        [
+                            "nama" => "Gunung"
+                        ],
+                        
                     ]
+                    ],
+                    [
+                        "nama" => "Hiburans",
+                        "subMenu" => [
+                            [
+                                "nama" => "Pantai"
+                            ],
+                            [
+                                "nama" => "Gunung"
+                            ],
+                            
+                        ]
+                    ]  
+                ],
+                
+    ],
+    [
+        "nama" => "Kuliner",
+        "subMenu" => [
+            [
+                "nama" => "Kuliner",
+                "subMenu" => [
+                    // [
+                    //     "nama" => "Pantai"
+                    // ],
+                    // [
+                    //     "nama" => "Gunung"
+                    // ],
+                    
                 ]
             ]
         ]
     ],
-    [
-        "nama" => "Kuliner"
-    ],
-    [
-        "nama" => "Hiburan"
-    ],
+    // [
+    //     "nama" => "Kuliner"
+    // ],
+    // [
+    //     "nama" => "Hiburan"
+    // ],
     [
         "nama" => "Tentang"
     ],
