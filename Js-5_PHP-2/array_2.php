32d593f0cdbaadd0227d6f0ba9bfbd2b75718e07
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-09-24 08:00:27 +0700

    part1-jbs5, add modifier css

--- Js-5_PHP-2/array_2.php
+++ Js-5_PHP-2/array_2.php
@@ -22,3 +22,58 @@ echo "Jenis Kelamin : {$Dosen['jenis_kelamin']} <br>";
 
 
 <!-- After Modify -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Array Associative</title>
+<style>
+    table {
+        width: 50%;
+        border-collapse: collapse;
+        margin: 25px 0;
+        font-size: 18px;
+        text-align: left;
+    }
+    th, td {
+        padding: 12px;
+        border-bottom: 1px solid #ddd;
+    }
+    th {
+        background-color: #f2f2f2;
+    }
+    tr:hover {
+        background-color: #f5f5f5;
+    }
+</style>
+</head>
+<body>
+<?php
+$Dosen = [
+    'nama' => 'Elok Nur Hamdana',
+    'domisili' => 'Malang',
+    'jenis_kelamin' => 'Perempuan'
+];
+
+echo "<table>
+        <tr>
+            <th>Attribute</th>
+            <th>Value</th>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>{$Dosen['nama']}</td>
+        </tr>
+        <tr>
+            <td>Domisili</td>
+            <td>{$Dosen['domisili']}</td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>{$Dosen['jenis_kelamin']}</td>
+        </tr>
+      </table>";
+?>
+</body>
+</html>
