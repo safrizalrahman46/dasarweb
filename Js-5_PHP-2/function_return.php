6978845ef43eea24b25c2dcbbf30965fab5b7b08
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-09-24 08:42:12 +0700

    question 8

--- Js-5_PHP-2/function_return.php
+++ Js-5_PHP-2/function_return.php
@@ -1,9 +1,31 @@
 <?php
-//membuat fungsi
+// //membuat fungsi
+// function hitungUmur($thn_lahir, $thn_sekarang) {
+// $umur = $thn_sekarang - $thn_lahir;
+// return $umur;
+// }
+// echo "Umur saya adalah ". hitungUmur(1988, 2023) ."tahun" //isi sesuai dengan tahun lahir kalian
+
+// Modify
+
+// Membuat fungsi untuk menghitung umur
 function hitungUmur($thn_lahir, $thn_sekarang) {
-$umur = $thn_sekarang - $thn_lahir;
-return $umur;
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
-echo "Umur saya adalah ". hitungUmur(1988, 2023) ."tahun" //isi sesuai dengan tahun lahir kalian
+
+// Membuat fungsi perkenalan
+function perkenalan($nama, $salam = "Assalamualaikum") {
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/>";
+    
+    // Memanggil fungsi lain
+    echo "Saya berusia " . hitungUmur(1988, 2023) . " tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+// Memanggil fungsi perkenalan
+perkenalan("Elok");
+
 
 ?>
