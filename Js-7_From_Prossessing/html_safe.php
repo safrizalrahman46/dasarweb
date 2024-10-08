dbfa7ea15f0be2592cd1a9e36b3974cc9db984c6
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-08 08:57:19 +0700

    js_7_Form_prosessing_No 4.1 modify

--- Js-7_From_Prossessing/html_safe.php
+++ Js-7_From_Prossessing/html_safe.php
@@ -1,4 +1,27 @@
-<?php
-$input = $_POST['input'];
-$input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-?>
+
+// $input = $_POST['input'];
+// $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HTML Injection Prevention</title>
+</head>
+<body>
+    <form method="post" action="html_safe.php">
+        <label for="input">Input: </label>
+        <input type="text" name="input" id="input" required><br><br>
+        <input type="submit" value="Submit">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $input = $_POST['input'];
+        $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+        echo "Sanitized Input: " . $input;
+    }
+    ?>
+</body>
+</html>
