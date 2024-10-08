d7eb68cfce58eaf144e59cc9d263805fed13bf03
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-08 08:17:59 +0700

    js_7_Form_prosessing_No 1.2

--- Js-7_From_Prossessing/isset.php
+++ Js-7_From_Prossessing/isset.php
@@ -1,8 +1,32 @@
 <?php
-$umur = 20; // Assign a value to $umur
+// $umur = 20; // Assign a value to $umur
+// if (isset($umur) && $umur >= 18) {
+//     echo "Anda sudah dewasa.";
+// } else {
+//     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
+// }
+
+//1.2
+// $data = array("nama" => "Jane", "usia" => 25);
+// if (isset($data["nama"])) {
+//     echo "Nama: " . $data["nama"];
+// } else {
+//     echo "Variabel 'nama' tidak ditemukan dalam array.";
+// }
+
+//1.2 full
+$umur;
 if (isset($umur) && $umur >= 18) {
-    echo "Anda sudah dewasa.";
+    echo "Anda sudah dewasa.<br>";
 } else {
-    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.<br>";
 }
+
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"] . "<br>";
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.<br>";
+}
+
 ?>
