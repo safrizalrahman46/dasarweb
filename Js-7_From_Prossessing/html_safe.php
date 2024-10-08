6e2ab430e9f3a0e78d0a2925866bef39dfcac9d8
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-08 08:58:39 +0700

    js_7_Form_prosessing_No 4.2

--- Js-7_From_Prossessing/html_safe.php
+++ Js-7_From_Prossessing/html_safe.php
@@ -1,9 +1,3 @@
-
-// $input = $_POST['input'];
-// $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,6 +7,10 @@
     <form method="post" action="html_safe.php">
         <label for="input">Input: </label>
         <input type="text" name="input" id="input" required><br><br>
+
+        <label for="email">Email: </label>
+        <input type="email" name="email" id="email" required><br><br>
+
         <input type="submit" value="Submit">
     </form>
 
@@ -20,7 +18,14 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $input = $_POST['input'];
         $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-        echo "Sanitized Input: " . $input;
+        echo "Sanitized Input: " . $input . "<br>";
+
+        $email = $_POST['email'];
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Valid Email: " . $email . "<br>";
+        } else {
+            echo "Invalid Email Format.<br>";
+        }
     }
     ?>
 </body>
