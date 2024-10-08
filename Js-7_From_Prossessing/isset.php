26720bdf37de47ac3f1670c4580e6f073fd47a19
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-08 08:14:58 +0700

    js_7_Form_prosessing_No 1.1

---
+++ Js-7_From_Prossessing/isset.php
@@ -0,0 +1,8 @@
+<?php
+$umur = 20; // Assign a value to $umur
+if (isset($umur) && $umur >= 18) {
+    echo "Anda sudah dewasa.";
+} else {
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
+}
+?>
