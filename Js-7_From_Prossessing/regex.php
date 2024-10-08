9e5df932a159927db091afbb8eff414d1eec1dcd
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-08 09:05:50 +0700

    js_7_Form_prosessing_No 5.1

---
+++ Js-7_From_Prossessing/regex.php
@@ -0,0 +1,9 @@
+<?php
+$pattern = '/[a-z]/'; // Cocokkan huruf kecil.
+$text = 'This is a Sample Text.';
+if (preg_match($pattern, $text)) {
+    echo "Huruf kecil ditemukan!<br>";
+} else {
+    echo "Tidak ada huruf kecil!<br>";
+}
+?>
