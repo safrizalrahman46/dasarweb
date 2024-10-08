3e9583c5c99ffdf41f4721ed631a51bf961bebd0
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-08 09:11:58 +0700

    js_7_Form_prosessing_No 5.4

--- Js-7_From_Prossessing/regex.php
+++ Js-7_From_Prossessing/regex.php
@@ -25,6 +25,29 @@
 // }
 
 //5.3
+// $pattern = '/[a-z]/'; // Cocokkan huruf kecil.
+// $text = 'This is a Sample Text.';
+// if (preg_match($pattern, $text)) {
+//     echo "Huruf kecil ditemukan!<br>";
+// } else {
+//     echo "Tidak ada huruf kecil!<br>";
+// }
+
+// $pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
+// $text = 'There are 123 apples.';
+// if (preg_match($pattern, $text, $matches)) {
+//     echo "Cocokkan: " . $matches[0] . "<br>";
+// } else {
+//     echo "Tidak ada yang cocok!<br>";
+// }
+
+// $pattern = '/apple/';
+// $replacement = 'banana';
+// $text = 'I like apple pie.';
+// $new_text = preg_replace($pattern, $replacement, $text);
+// echo $new_text . "<br>"; 
+
+//5.4
 $pattern = '/[a-z]/'; // Cocokkan huruf kecil.
 $text = 'This is a Sample Text.';
 if (preg_match($pattern, $text)) {
@@ -45,5 +68,13 @@ $pattern = '/apple/';
 $replacement = 'banana';
 $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
-echo $new_text . "<br>"; 
+echo $new_text . "<br>"; // Output: "I like banana pie."
+
+$pattern = '/go*d/'; // Cocokkan "god", "good", "gooood", dll.
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0] . "<br>";
+} else {
+    echo "Tidak ada yang cocok!<br>";
+}
 ?>
