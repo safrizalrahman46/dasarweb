48403aa4b7e86716b86bafd7486034c24f60b299
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-08 08:21:27 +0700

    js_7_Form_prosessing_No 2.1

---
+++ Js-7_From_Prossessing/empty.php
@@ -0,0 +1,8 @@
+<?php
+$myArray = array(); // Array kosong
+if (empty($myArray)) {
+    echo "Array tidak terdefinisi atau kosong.<br>";
+} else {
+    echo "Array terdefinisi dan tidak kosong.<br>";
+}
+?>
