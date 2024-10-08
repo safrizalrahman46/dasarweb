05b0c56ad172dad0bba30a11c86abc1f7cfe7752
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-08 08:51:58 +0700

    js_7_Form_prosessing_No 2.1

--- Js-7_From_Prossessing/empty.php
+++ Js-7_From_Prossessing/empty.php
@@ -1,8 +1,26 @@
 <?php
+// $myArray = array(); // Array kosong
+// if (empty($myArray)) {
+//     echo "Array tidak terdefinisi atau kosong.<br>";
+// } else {
+//     echo "Array terdefinisi dan tidak kosong.<br>";
+// }
+
+//2.2
+
 $myArray = array(); // Array kosong
 if (empty($myArray)) {
     echo "Array tidak terdefinisi atau kosong.<br>";
 } else {
     echo "Array terdefinisi dan tidak kosong.<br>";
 }
+
+$nonExistentVar; // Variabel belum diinisialisasi
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.<br>";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.<br>";
+}
+
+
 ?>
