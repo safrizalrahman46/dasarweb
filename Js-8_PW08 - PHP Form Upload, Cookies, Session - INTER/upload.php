1663b6930ef48af81a4ff4d9dc98d6fdf81cf881
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-15 08:18:11 +0700

    Js_8_PHP Form Upload, Cookies, Session - INTER_1.4

--- Js-8_PW08 - PHP Form Upload, Cookies, Session - INTER/upload.php
+++ Js-8_PW08 - PHP Form Upload, Cookies, Session - INTER/upload.php
@@ -32,7 +32,7 @@ if (isset($_POST["submit"])) {
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
     $allowedExtensions = array("txt", "pdf", "doc", "docx");
-    $maxsize = 3 * 1024 * 1024;
+    $maxsize = 100  * 1024 * 1024;
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
