92f07c167029a303903b5df33d59dbb612f27150
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-15 07:42:49 +0700

    commit 1.1 -upload php

--- Js-8_PW08 - PHP Form Upload, Cookies, Session - INTER/Create an HTML form to upload a file/upload.php
+++ Js-8_PW08 - PHP Form Upload, Cookies, Session - INTER/Create an HTML form to upload a file/upload.php
@@ -1,7 +1,17 @@
 <?php
+// if (isset($_POST["submit"])) {
+//     $targetDirectory = "uploads/"; // Direktori tujuan untuk menyimpan file
+//     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+//     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+//         echo "File berhasil diunggah.";
+//     } else {
+//         echo "Gagal mengunggah file.";
+//     }
+// }
 if (isset($_POST["submit"])) {
     $targetDirectory = "uploads/"; // Direktori tujuan untuk menyimpan file
     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+    echo "Target File: " . $targetFile . "<br>";
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
         echo "File berhasil diunggah.";
     } else {
