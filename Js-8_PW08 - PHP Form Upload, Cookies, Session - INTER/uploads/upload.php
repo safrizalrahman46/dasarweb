509e709dda2db7b171f30f7f60c205d646f2ab36
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-15 08:11:38 +0700

    Js_8_PHP Form Upload, Cookies, Session - INTER_1.2

--- Js-8_PW08 - PHP Form Upload, Cookies, Session - INTER/uploads/upload.php
+++ Js-8_PW08 - PHP Form Upload, Cookies, Session - INTER/uploads/upload.php
@@ -17,13 +17,20 @@ if (isset($_POST["submit"])) {
     $allowedExtensions = array("jpg", "jpeg", "png", "gif");
     $maxsize = 5 * 1024 * 1024;
 
-    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
-        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File berhasil diunggah.";
-        } else {
-            echo "Gagal mengunggah file.";
-        }
+    // if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+    //     if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+    //         echo "File berhasil diunggah.";
+    //     } else {
+    //         echo "Gagal mengunggah file.";
+    //     }
+    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+        echo "File berhasil diunggah.";
+        echo "<img src='" . $targetfile . "' width='200'>";
+    } else {
+        echo "Gagal mengunggah file.";
+    }
+    
     } else {
         echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
     }
-}
+
