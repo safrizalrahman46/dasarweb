2f78cf3bd6291ba502d80989d260eb639e6736a7
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-10-15 08:26:58 +0700

    Js_8_PHP Form Upload, Cookies, Session - INTER_2.2

--- Js-8_PW08 - PHP Form Upload, Cookies, Session - INTER/Practical Section 2. Multi Upload File/form_multiupload.php
+++ Js-8_PW08 - PHP Form Upload, Cookies, Session - INTER/Practical Section 2. Multi Upload File/form_multiupload.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
     <title>Multiupload Dokumen</title>
@@ -7,6 +7,20 @@
     <h2>Unggah Dokumen</h2>
     <form action="proses_upload.php" method="post" enctype="multipart/form-data">
         <input type="file" name="files[]" multiple="multiple" accept=".pdf, .doc, .docx">
+        <input type="submit" value="Unggah">
+    </form>
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Multiupload Gambar</title>
+</head>
+<body>
+    <h2>Unggah Gambar</h2>
+    <form action="proses_upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="files[]" multiple="multiple" accept=".jpg, .jpeg, .png, .gif">
         <input type="submit" value="Unggah">
     </form>
 </body>
--- Js-8_PW08 - PHP Form Upload, Cookies, Session - INTER/Practical Section 2. Multi Upload File/proses_upload.php
+++ Js-8_PW08 - PHP Form Upload, Cookies, Session - INTER/Practical Section 2. Multi Upload File/proses_upload.php
@@ -1,6 +1,32 @@
 <?php
+// // Lokasi penyimpanan file yang diunggah
+// $targetDirectory = "documents/";
+
+// // Periksa apakah direktori penyimpanan ada, jika tidak maka buat
+// if (!file_exists($targetDirectory)) {
+//     mkdir($targetDirectory, 0777, true);
+// }
+
+// if ($_FILES['files']['name'][0]) {
+//     $totalFiles = count($_FILES['files']['name']);
+
+//     // Loop melalui semua file yang diunggah
+//     for ($i = 0; $i < $totalFiles; $i++) {
+//         $fileName = $_FILES['files']['name'][$i];
+//         $targetFile = $targetDirectory . $fileName;
+
+//         // Pindahkan file yang diunggah ke direktori penyimpanan
+//         if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+//             echo "File $fileName berhasil diunggah.<br>";
+//         } else {
+//             echo "Gagal mengunggah file $fileName.<br>";
+//         }
+//     }
+// } else {
+//     echo "Tidak ada file yang diunggah.";
+// }
 // Lokasi penyimpanan file yang diunggah
-$targetDirectory = "documents/";
+$targetDirectory = "images/";
 
 // Periksa apakah direktori penyimpanan ada, jika tidak maka buat
 if (!file_exists($targetDirectory)) {
@@ -14,12 +40,19 @@ if ($_FILES['files']['name'][0]) {
     for ($i = 0; $i < $totalFiles; $i++) {
         $fileName = $_FILES['files']['name'][$i];
         $targetFile = $targetDirectory . $fileName;
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
 
-        // Pindahkan file yang diunggah ke direktori penyimpanan
-        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-            echo "File $fileName berhasil diunggah.<br>";
+        // Pindahkan file yang diunggah ke direktori penyimpanan jika jenis file valid
+        if (in_array($fileType, $allowedExtensions)) {
+            if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+                echo "File $fileName berhasil diunggah.<br>";
+                echo "<img src='" . $targetFile . "' width='200'><br>";
+            } else {
+                echo "Gagal mengunggah file $fileName.<br>";
+            }
         } else {
-            echo "Gagal mengunggah file $fileName.<br>";
+            echo "File $fileName bukan gambar yang valid.<br>";
         }
     }
 } else {
