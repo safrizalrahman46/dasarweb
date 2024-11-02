7a1bbe8cd33bdfee2c11673937f446380c1c8869
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-02 14:44:19 +0000

    Js-10.2 - PHP - CRUD-3.1

--- Js-10.2 - PHP - CRUD/proses.php
+++ Js-10.2 - PHP - CRUD/proses.php
@@ -2,6 +2,8 @@
 include('koneksi.php');
 
 $aksi = $_GET['aksi'];
+
+// Mengambil data dari POST
 $nama = $_POST['nama'];
 $jenis_kelamin = $_POST['jenis_kelamin'];
 $alamat = $_POST['alamat'];
@@ -15,6 +17,21 @@ if ($aksi == 'tambah') {
         exit();
     } else {
         echo "Gagal menambahkan data: " . mysqli_error($koneksi);
+    }
+} elseif ($aksi == 'ubah') {
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+
+        $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id=$id";
+
+        if (mysqli_query($koneksi, $query)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gagal mengupdate data: " . mysqli_error($koneksi);
+        }
+    } else {
+        echo "ID tidak valid.";
     }
 }
 
